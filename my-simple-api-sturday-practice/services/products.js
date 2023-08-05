4c1f22c88d33ca5ddd69f07222294175f66d98c4
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-05 12:39:27 +0500

    readfile

--- my-simple-api-sturday-practice/services/products.js
+++ my-simple-api-sturday-practice/services/products.js
@@ -2,5 +2,7 @@ import fs from "fs"
 import path from "path"
 const filePath = path.join(process.cwd(), "my-simple-api-sturday-practice", "data","products.json")
  export  function getAll() {
+    const data = fs.readFileSync(filePath);
+    // console.log()
 
  }
