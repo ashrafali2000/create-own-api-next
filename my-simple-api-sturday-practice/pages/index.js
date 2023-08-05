1c365deeb2ae1c5de891da283819615e424b996b
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-05 14:53:56 +0500

    create todo type

--- my-simple-api-sturday-practice/pages/index.js
+++ my-simple-api-sturday-practice/pages/index.js
@@ -1,8 +1,35 @@
+import { getAll } from "@/services/products"
 
-export default function Home() {
+export default function Home(props) {
+  const {title, products} = props;
+  if(!products) {
+    return <div>
+      Loading....
+    </div>
+  }
+  const productsList = products.map(product => {
+    return (<li key={product.id}>
+      <p>{product.title}</p>
+    </li>)
+  })
   return (
     <>
    <h1>It is my API projects</h1>
+   <h2>{title}</h2>
+   {productsList}
     </>
   )
+}
+
+
+export async function getStaticeProps() {
+  const data =  getAll()
+  console.log(data);
+  return {
+    props : {
+      title: "hello",
+      products: data,
+
+    }
+  }
 }
