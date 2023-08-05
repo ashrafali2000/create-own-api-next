c3376fc290b8df09313d22bfafdc07fb4c66ed51
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-05 12:46:10 +0500

    correct the path

--- my-simple-api-sturday-practice/pages/api/products/index.js
+++ my-simple-api-sturday-practice/pages/api/products/index.js
@@ -1,3 +1,5 @@
+import { getAll } from "@/services/products";
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const products = [
   {
@@ -556,6 +558,7 @@ const products = [
 
 export default function handler(req, res) {
   if(req.method === "GET"){
+    getAll()
     res.status(200).json(products)
   }
   return res.status(404).send();
--- my-simple-api-sturday-practice/services/products.js
+++ my-simple-api-sturday-practice/services/products.js
@@ -1,8 +1,8 @@
 import fs from "fs"
 import path from "path"
-const filePath = path.join(process.cwd(), "my-simple-api-sturday-practice", "data","products.json")
+const filePath = path.join(process.cwd(), "data","products.json")
  export  function getAll() {
     const data = fs.readFileSync(filePath);
-    // console.log()
+    console.log(JSON.parse(data))
 
  }
